953093da4ee480e4aff708f67694277a2dcb07c6
msimon42 <msimon@uab.edu>
2020-03-02 11:34:44 -0700

    Add munchies controller

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::MunchiesController < ApplicationController
   def show
-    
+    render json: MunchieSerializer.new(Munchie.new(params[:start], params[:end], params[:food]))
   end
 end
