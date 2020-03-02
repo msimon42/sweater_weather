7b8c66773117f2124c6e15a449049a587e7fecbd
msimon42 <msimon@uab.edu>
2020-03-02 11:34:16 -0700

    Fix typos

--- spec/requests/api/v1/restaurant_data_spec.rb
+++ spec/requests/api/v1/restaurant_data_spec.rb
@@ -6,13 +6,13 @@ RSpec.describe 'munchie endpoint' do
       get "/api/v1/munchies?start=denver,co&end=pueblo,co&food=chinese"
 
       expect(response).to be_successful
-      data = JSON.parse(respnse.body)['data']
+      data = JSON.parse(response.body)['data']
 
-      expect(data['attributes']]'end_location').to eq('Pueblo, CO, USA')
-      expect(data['attributes']['travel_time']).to eq('1 hour, 48 mins')
+      expect(data['attributes']['end_location']).to eq('Pueblo, CO, USA')
+      expect(data['attributes']['travel_time']).to eq('1 hour 48 mins')
       #VCR is having issues with this test; test will probably fail due to changing weather.
       expect(data['attributes']['forecast']).to eq('Clear')
-      expect(data['attributes']['restaurant']).to eq({name: "Kan's Kitchen", address: "1620 S Prairie AvePueblo, CO 81005"})
+      expect(data['attributes']['restaurant']).to eq({'name' => "Kan's Kitchen", 'address' => "1620 S Prairie AvePueblo, CO 81005"})
     end
   end
 end
