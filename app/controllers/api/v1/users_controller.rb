1a4e5a327bae04938355a469109228331338add7
msimon42 <msimon@uab.edu>
2020-03-02 16:41:34 -0700

    Add action for creating user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,15 @@
 class Api::V1::UsersController < ApplicationController
   def create
+    user = User.create(user_params)
+    if user.save
+      render json: {'api_key' => user.api_key}
+    else
+      render json: {'errors' => user.errors.full_messages.to_sentence}, status: 400
+    end     
+  end
 
-  end   
+  private
+    def user_params
+      params.permit(:email, :password)
+    end
 end
