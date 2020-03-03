924fd4d098a108d960dac82a8a915776e39114be
msimon42 <msimon@uab.edu>
2020-03-02 19:20:43 -0700

    Add test for user login

---
+++ spec/requests/api/v1/user_login_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'create session' do
+  before :each do
+    @user = create :user, password: 'password'
+  end
+
+  it 'works' do
+    post '/api/v1/sessions', params: {
+      email: 'test@email.com',
+      password: 'password'
+    }
+
+    expect(response).to be_successful
+    body = JSON.parse(response.body)
+
+    expect(body).to eq({'api_key' => User.api_key})
+  end
+end
