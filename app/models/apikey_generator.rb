5f39066212cfeaeb97b4790325856523ff14f1f4
msimon42 <msimon@uab.edu>
2020-03-02 17:08:49 -0700

    Limit length to 25

--- app/models/apikey_generator.rb
+++ app/models/apikey_generator.rb
@@ -2,6 +2,6 @@ class ApikeyGenerator
   def self.generate
     letters = ('a'..'z').to_a
     numbers = (0..9).to_a
-    (letters + numbers).shuffle.join
+    (letters + numbers).sample(25).shuffle.join
   end
 end
