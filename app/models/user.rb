760e5262bfb3b31721f512064d39d1752d14c0d4
msimon42 <msimon@uab.edu>
2020-03-02 17:36:04 -0700

    Add method to generate api key

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   validates_uniqueness_of :email
   has_secure_password
 
-  
+  def generate_api_key
+    key = ApikeyGenerator.generate
+    self.update(api_key: key)
+  end
 end
