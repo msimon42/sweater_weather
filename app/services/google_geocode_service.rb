f2bd10679aa165e84a5429e8bc3e0dde486fa3d8
msimon42 <msimon@uab.edu>
2020-03-03 21:05:39 -0700

    Change return values

--- app/services/google_geocode_service.rb
+++ app/services/google_geocode_service.rb
@@ -1,18 +1,14 @@
 class GoogleGeocodeService
-  def get_coordinates(location)
+  def geocode_endpoint(location)
     params = {address: location, key: ENV['GOOGLE_KEY']}
     endpoint = 'maps/api/geocode/json'
     response = json_parse(endpoint, params)
-    coordinates = response['results'][0]['geometry']['location']
-    location = response['results'][0]['formatted_address']
-    {coordinates: "#{coordinates['lat']},#{coordinates['lng']}", location: location}
   end
 
-  def travel_time(origin, destination)
+  def directions_endpoint(origin, destination)
     params = {origin: origin, destination: destination, key: ENV['GOOGLE_KEY']}
     endpoint = 'maps/api/directions/json'
     response = json_parse(endpoint, params)
-    {text: response['routes'][0]['legs'][0]['duration']['text'], value: response['routes'][0]['legs'][0]['duration']['value']}
   end
 
   private
