c81d71aea374ae57c64a7b9601744a2a43000a73
msimon42 <msimon@uab.edu>
2020-03-03 21:05:21 -0700

    Update for refactor

--- spec/poros/forecast_generator_spec.rb
+++ spec/poros/forecast_generator_spec.rb
@@ -7,35 +7,31 @@ RSpec.describe ForecastGenerator do
     end
 
     it 'current_temperature' do
-      expect(@forecast.current_temperature).to eq(34.26)
+      expect(@forecast.current_weather[:current_temperature]).to eq(34.26)
     end
 
     it 'current_summary' do
-      expect(@forecast.current_summary).to eq('Flurries')
-    end
-
-    it 'formatted_location' do
-      expect(@forecast.formatted_location).to eq('Denver, CO, USA')
+      expect(@forecast.current_weather[:current_summary]).to eq('Flurries')
     end
 
     it 'today' do
-      expect(@forecast.today).to eq('Foggy later this evening and tonight.')
+      expect(@forecast.current_weather[:today]).to eq('Foggy later this evening and tonight.')
     end
 
     it 'tomorrow' do
-      expect(@forecast.tomorrow).to eq('Clear throughout the day.')
+      expect(@forecast.current_weather[:tomorrow]).to eq('Clear throughout the day.')
     end
 
     it 'humidity' do
-      expect(@forecast.humidity).to eq(0.82)
+      expect(@forecast.current_weather[:humidity]).to eq(0.82)
     end
 
     it 'visibility' do
-      expect(@forecast.visibility).to eq(5.478)
+      expect(@forecast.current_weather[:visibility]).to eq(5.478)
     end
 
     it 'uv_index' do
-      expect(@forecast.uv_index).to eq(0)
+      expect(@forecast.current_weather[:uv_index]).to eq(0)
     end
 
     it 'hourly_forecast' do
