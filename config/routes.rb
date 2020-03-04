bd131eabadcb1150c909382e7de2602820797010
msimon42 <msimon@uab.edu>
2020-03-03 21:41:45 -0700

    Add route for road_trip endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
       post '/users', to: 'users#create'
       post '/sessions', to: 'sessions#create'
+      post '/road_trip', to: 'roadtrip#show'
     end
   end
 end
